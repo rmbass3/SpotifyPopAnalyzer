57913b5213d1756e342fccad9492f7ffa41eee3a
Ryan <rmbass3@gmail.com>
2022-04-28 20:16:12 -0400

    Display favorite tracks

--- spotify_analyzer/src/components/FavoriteT.js
+++ spotify_analyzer/src/components/FavoriteT.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, {useEffect} from "react";
 import axios from "axios";
 
 function FavoriteT({token, favoriteT, setFavoriteT}){
@@ -24,11 +24,30 @@ function FavoriteT({token, favoriteT, setFavoriteT}){
     }
   }, [token, setFavoriteT])
   
+  // useEffect(() => {
+  //   const displayFavoriteT = () => {
 
+  //   }
+  //   if (favoriteT != null){
+  //     displayFavoriteT()
+  //   }
+    
+  // }, [favoriteT])
+  const displayFavoriteT = () => {
+    if (favoriteT.items != null){
+      return favoriteT.items.map(track => (
+        <div key={track.id} className="track-container d-inline-flex justify-content-center align-items-center">
+          <h4 className="track-name text-light">{track.name}</h4>
+        </div>
+      ))
+    }
+  }
 
   return(
     <div>
-
+      <div className="tracklist d-flex flex-wrap container fluid mt-5">
+        {displayFavoriteT()}
+      </div>
     </div>
   )
 }
