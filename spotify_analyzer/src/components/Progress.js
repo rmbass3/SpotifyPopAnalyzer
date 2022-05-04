f5de98e8ce2dcd42728cf35dfb46da5638058f6f
rmbass3@gmail.com <rmbass3@gmail.com>
2022-05-04 18:31:04 -0400

    Added accordion

--- spotify_analyzer/src/components/Progress.js
+++ spotify_analyzer/src/components/Progress.js
@@ -1,5 +1,7 @@
 import React, {useEffect} from "react";
 import Progressbar from 'react-js-progressbar'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleDown } from '@fortawesome/free-solid-svg-icons'
 
 function Progress({percent, setPercent, user, token, favoriteT}) {
 
@@ -16,6 +18,12 @@ function Progress({percent, setPercent, user, token, favoriteT}) {
     }
   }, [user, token, favoriteT, setPercent])
 
+  /*
+  if average song popularity less than or equal to 25% rating says "hipster" 
+  if greater than 25 but less than or equal to 50 rating says "based"
+  if greater than 50 but less than or equal to 75 rating says "centrist"
+  if greater than 75 rating says "normie"
+  */
 
 
   return ((user && token) ?
@@ -37,13 +45,62 @@ function Progress({percent, setPercent, user, token, favoriteT}) {
             }}
           />
         </div>
-        <div className="progress-comment col-7 d-flex justify-content-center align-items-center mb-5">
-          <ul className="list-group w-100 mx-5">
-            <li className="list-group-item text-light bg-dark"><h5>Average Song Popularity: <b>-</b></h5></li>
-            <li className="list-group-item text-light bg-dark"><h5>Most Popular Song: <b>-</b></h5></li>
-            <li className="list-group-item text-light bg-dark"><h5>Least Popular Song: <b>-</b></h5></li>
-            <li className="list-group-item text-light bg-dark"><h5>Rating: <b>-</b></h5></li>
-          </ul>
+        <div className="progress-comment col-7 d-flex flex-column justify-content-center align-items-center mb-5">
+          <h1 className="rating text-light mb-5">Rating: Based</h1>
+          <div className="accordion w-100" id="accordionExample">
+            <div className="accordion-item">
+              <h2 className="accordion-header" id="headingOne">
+                <button className="accordion-button position-relative" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                  Popularity Score:
+                  <FontAwesomeIcon icon={faAngleDown} className="position-absolute top-50 translate-middle"/>
+                </button>
+              </h2>
+              <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                <div className="accordion-body">
+                  <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                </div>
+              </div>
+            </div>
+            <div className="accordion-item">
+              <h2 className="accordion-header" id="headingTwo">
+                <button className="accordion-button position-relative collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                  Your Most Popular Song(s)
+                  <FontAwesomeIcon icon={faAngleDown} className="position-absolute top-50 translate-middle"/>
+                </button>
+              </h2>
+              <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                <div className="accordion-body">
+                  <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                </div>
+              </div>
+            </div>
+            <div className="accordion-item">
+              <h2 className="accordion-header" id="headingThree">
+                <button className="accordion-button position-relative collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                  Your Least Popular Song(s)
+                  <FontAwesomeIcon icon={faAngleDown} className="position-absolute top-50 translate-middle"/>
+                </button>
+              </h2>
+              <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                <div className="accordion-body">
+                  <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                </div>
+              </div>
+            </div>
+            <div className="accordion-item">
+              <h2 className="accordion-header" id="headingFour">
+                <button className="accordion-button position-relative collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseThree">
+                  Stats
+                  <FontAwesomeIcon icon={faAngleDown} className="position-absolute top-50 translate-middle"/>
+                </button>
+              </h2>
+              <div id="collapseFour" className="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#accordionExample">
+                <div className="accordion-body">
+                  <strong>This is the fourth item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
